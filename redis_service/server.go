f1d7eb9af707b434ea2c50ecef451c57ee410e27
Alexey Rubanik <rubanik.aleksey@gmail.com>
2021-11-29 17:10:34 +0300

    Improve searching/storing processs and refactoring

--- redis_service/server.go
+++ redis_service/server.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 	"github.com/hashicorp/go-hclog"
@@ -31,6 +32,28 @@ func (m MyError) Error() string {
 
 const redisAdress = "localhost:6379"
 
+//Clearing Redis (only for testing purpose)
+func ClearRedis() {
+	conn, err := redis.Dial("tcp", redisAdress)
+	if err != nil {
+		log.Fatal("Error when connecting to Redis service")
+	}
+	defer conn.Close()
+	_, err = conn.Do("FLUSHALL")
+	if err != nil {
+		log.Fatal("Error when clearing database")
+	}
+}
+
+//helper function for searching
+func B2S(bs []uint8) string {
+	b := make([]byte, len(bs))
+	for i, v := range bs {
+		b[i] = byte(v)
+	}
+	return string(b)
+}
+
 func (s *Server) StoreWorkload(ctx context.Context, req *protobuff.StoreWorkloadRequest) (*protobuff.ErrorMsg, error) {
 	conn, err := redis.Dial("tcp", redisAdress)
 	errMsg := "OK"
@@ -39,7 +62,12 @@ func (s *Server) StoreWorkload(ctx context.Context, req *protobuff.StoreWorkload
 		return &protobuff.ErrorMsg{Error: errMsg}, err
 	}
 	defer conn.Close()
-	v, err := conn.Do("HSET", req.GetStation(), "WorkLoad", req.GetWorkload())
+
+	//version of storing that is using for testing
+	//v, err := conn.Do("SET", strings.TrimSpace(req.GetStation()), req.GetWorkload())
+
+	v, err := conn.Do("SET", req.GetStation()+time.Now().Format("01-02-2006"), req.GetWorkload())
+
 	fmt.Println(v)
 	if err != nil {
 		errMsg = "Cannot add data to Redis"
@@ -64,15 +92,31 @@ func (s *Server) SearchWorkload(req *protobuff.Stations, srv protobuff.RedisServ
 				log.Printf("Cannot connect to redis")
 			}
 			defer conn.Close()
-			wl, err := redis.String(conn.Do("HGET", v, "WorkLoad"))
+			keys, err := redis.Values(conn.Do("SCAN", 0, "MATCH", v+"*"))
 			if err != nil {
-				log.Printf("Got error %v when searching for %v", err, v)
+				log.Printf("Error HSCAN %v", err)
 			}
-			msg := &protobuff.SearchWorkloadResponse{StationName: v, Workload: wl}
-			if err := srv.Send(msg); err != nil {
-				log.Printf("send error %v", err)
+			for i, key := range keys {
+				if i == 0 {
+					continue
+				}
+				typedkey := key.([]interface{})
+				tmp := typedkey[0].([]uint8)
+				_ = tmp
+				strkey := B2S(tmp)
+				fmt.Println(strkey)
+				wl, err := redis.String(conn.Do("GET", strkey))
+				if err != nil {
+					log.Printf("Got error %v when searching for %v", err, v)
+				}
+				msg := &protobuff.SearchWorkloadResponse{StationName: strkey, Workload: wl}
+				if err := srv.Send(msg); err != nil {
+					log.Printf("send error %v", err)
+				}
+				s.log.Info("Send workload for station : %s", strkey)
+
 			}
-			s.log.Info("Send workload for station : %s", v)
+
 		}(v)
 	}
 	wg.Wait()
