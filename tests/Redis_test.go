d9eeb68d32694b60ebc9669e2a0469497c83324b
Alexey Rubanik <rubanik.aleksey@gmail.com>
2021-11-29 17:08:44 +0300

    Completed Redis tests

--- tests/Redis_test.go
+++ tests/Redis_test.go
@@ -1,7 +1,7 @@
 package tests
 
 import (
-	redisworkload "RailwayStationsWorkload_micro/redis_service"
+	redisservice "RailwayStationsWorkload_micro/redis_service"
 	redisProtobuff "RailwayStationsWorkload_micro/redis_service/protobuff"
 	"context"
 	"io"
@@ -11,8 +11,10 @@ import (
 )
 
 // should be used only with no data in Redis to pass this test without accidential matches for test requests
+// so using ClearRedis() to achieve that
 func TestMissingData(t *testing.T) {
-	redisworkload.NewRedis()
+	redisservice.ClearRedis()
+	redisservice.NewRedis()
 	tests := []struct {
 		request  redisProtobuff.Stations
 		response []redisProtobuff.SearchWorkloadResponse
@@ -36,7 +38,7 @@ func TestMissingData(t *testing.T) {
 		},
 	}
 
-	conn, err := grpc.Dial(redisworkload.Redisport, grpc.WithBlock(), grpc.WithInsecure())
+	conn, err := grpc.Dial(redisservice.Redisport, grpc.WithBlock(), grpc.WithInsecure())
 	if err != nil {
 		t.Error("Error when connecting to Redis service")
 	}
@@ -67,5 +69,87 @@ func TestMissingData(t *testing.T) {
 			cnt++
 		}
 
+	}
+}
+
+func TestRedisStoreAndSearch(t *testing.T) {
+	redisservice.ClearRedis()
+	redisservice.NewRedis()
+	lengthCases := 3
+	fillingCmds := []struct {
+		request      redisProtobuff.StoreWorkloadRequest
+		ErrorMessage string
+	}{
+		{
+			request:      redisProtobuff.StoreWorkloadRequest{Station: "Дачное_11.11.2021", Workload: "something"},
+			ErrorMessage: "OK",
+		},
+		{
+			request:      redisProtobuff.StoreWorkloadRequest{Station: "Лигово_15.11.2021", Workload: "Other something"},
+			ErrorMessage: "OK",
+		},
+		{
+			request:      redisProtobuff.StoreWorkloadRequest{Station: "Дачное_15.11.2021", Workload: "Another something"},
+			ErrorMessage: "OK",
+		},
+	}
+	searchCmds := []struct {
+		request  redisProtobuff.Stations
+		response []redisProtobuff.SearchWorkloadResponse
+	}{
+		{
+			request:  redisProtobuff.Stations{StationsNames: "Дачное"},
+			response: []redisProtobuff.SearchWorkloadResponse{{StationName: "Дачное_11.11.2021", Workload: "something"}},
+		},
+		{
+			request:  redisProtobuff.Stations{StationsNames: "Лигово"},
+			response: []redisProtobuff.SearchWorkloadResponse{{StationName: "Лигово_15.11.2021", Workload: "Other something"}},
+		},
+		{
+			request:  redisProtobuff.Stations{StationsNames: "Дачное "},
+			response: []redisProtobuff.SearchWorkloadResponse{{StationName: "Дачное_11.11.2021", Workload: "something"}, {StationName: "Дачное_15.11.2021", Workload: "Another something"}},
+		},
+	}
+	conn, err := grpc.Dial(redisservice.Redisport, grpc.WithBlock(), grpc.WithInsecure())
+	if err != nil {
+		t.Error("Error when connecting to Redis service")
+	}
+	defer conn.Close()
+	redis_client := redisProtobuff.NewRedisServiceClient(conn)
+	//storing data
+	for i := 0; i < lengthCases; i++ {
+		respMsg, err := redis_client.StoreWorkload(context.Background(), &fillingCmds[i].request)
+		if err != nil {
+			t.Errorf("Internal Redis error when storing %v", &fillingCmds[i])
+		}
+		if respMsg.Error != fillingCmds[i].ErrorMessage {
+			t.Errorf("Error %v returned from stroring %v", respMsg.Error, &fillingCmds[i].request)
+		}
+		//data stored now gonna search for it
+		stream, err := redis_client.SearchWorkload(context.Background(), &searchCmds[i].request)
+		if err != nil {
+			t.Error("Bad response from Redis service")
+		}
+		cnt := 0
+		for {
+			feature, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				t.Errorf("Error occures for test %v with error %v", searchCmds[i].request.StationsNames, err)
+			}
+			if feature.StationName != searchCmds[i].response[0].StationName {
+				if feature.Workload != searchCmds[i].response[1].Workload {
+					t.Errorf("Returned %v workload when it should be %v", feature.Workload, searchCmds[i].response[1].Workload)
+				} else if feature.StationName != searchCmds[i].response[1].StationName {
+					t.Errorf("%v when should be %v", feature.StationName, searchCmds[i].response[cnt].StationName)
+				}
+
+			} else if feature.Workload != searchCmds[i].response[0].Workload {
+				t.Errorf("Returned %v workload when it should be %v", feature.Workload, searchCmds[i].response[0].Workload)
+			}
+			cnt++
+		}
 	}
 }
